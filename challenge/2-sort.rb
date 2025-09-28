34c88a3770ab3b8392c839c1a4c0945cab62814b
muniira-03 <munirra.alsubaie@gmail.com>
2025-09-28 17:08:08 +0300

    .

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,1 +1,13 @@
+#!/usr/bin/env ruby
 
+args = ARGV
+
+sorted = args.sort do |a, b|
+  if a.to_i.to_s == a && b.to_i.to_s == b
+    a.to_i <=> b.to_i
+  else
+    a <=> b
+  end
+end
+
+sorted.each { |arg| puts arg }
